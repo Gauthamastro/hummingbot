375abb746be30f55d8c687946937b884e5dec645
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-09-29 20:44:22 +0530

    fix: adding custom strategies for load testing

--- hummingbot/connector/exchange/polkadex/polkadex_constants.py
+++ hummingbot/connector/exchange/polkadex/polkadex_constants.py
@@ -19,9 +19,9 @@ MIN_PRICE = Decimal(0.1)
 MIN_QTY = Decimal(0.1)
 TRADE_EVENT_TYPE = "trade"
 DIFF_EVENT_TYPE = "diff"
-GRAPHQL_ENDPOINT = "https://szzsvapgkjdurfl7ijvc3vtbba.appsync-api.eu-central-1.amazonaws.com/graphql"
-GRAPHQL_WSS_ENDPOINT = "https://szzsvapgkjdurfl7ijvc3vtbba.appsync-api.eu-central-1.amazonaws.com/graphql"
-GRAPHQL_API_KEY = "https://szzsvapgkjdurfl7ijvc3vtbba.appsync-api.eu-central-1.amazonaws.com/graphql"
+GRAPHQL_ENDPOINT = "https://hyl7f5nz7bbcxdhgidh75mau34.appsync-api.eu-central-1.amazonaws.com/graphql"
+GRAPHQL_WSS_ENDPOINT = "https://hyl7f5nz7bbcxdhgidh75mau34.appsync-api.eu-central-1.amazonaws.com/graphql"
+GRAPHQL_API_KEY = "https://hyl7f5nz7bbcxdhgidh75mau34.appsync-api.eu-central-1.amazonaws.com/graphql"
 
 POLKADEX_SS58_PREFIX = 88
 
